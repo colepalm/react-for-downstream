c8f849a62b184db14e2123283fd31d699732fe40
Cole Palm <palm.cole@gmail.com>
2016-09-18 17:25:26 -0700

    change up

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,140 @@
-var config = {
-   entry: './main.js',
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const dotenv = require('dotenv');
 
-   output: {
-      path:'./',
-      filename: 'index.js',
-   },
+const webpack = require('webpack');
+const fs = require ('fs');
+const path = require('path');
 
-   devServer: {
-      inline: true,
-      port: 8080
-   },
+const join = path.join;
+const resolve = path.resolve;
 
-   module: {
-      loaders: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel',
+const getConfig = require('hjs-webpack');
 
-            query: {
-               presets: ['es2015', 'react']
-            }
-         }
-      ]
-   }
+const isDev = NODE_ENV === 'development';
+const isTest = NODE_ENV === 'test';
+
+// devServer config
+const devHost   = process.env.HOST || 'localhost';
+const devPort   = process.env.PORT || 3000;
+
+const setPublicPath = process.env.SET_PUBLIC_PATH !== 'false';
+const publicPath  = (isDev && setPublicPath) ? `//${devHost}:${devPort}/` : '';
+
+const root = resolve(__dirname);
+const src = join(root, 'src');
+const modules = join(root, 'node_modules');
+const dest = join(root, 'dist');
+const css = join(src, 'styles');
+
+var config = getConfig({
+  in: join(__dirname, 'src/app.js'),
+  out: join(__dirname, 'dist')
+})
+
+// ENV variables
+const dotEnvVars = dotenv.config();
+const environmentEnv = dotenv.config({
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true
+});
+const envVariables =
+    Object.assign({}, dotEnvVars, environmentEnv);
+
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo, key) => {
+    const val = JSON.stringify(envVariables[key]);
+    memo[`__${key.toUpperCase()}__`] = val;
+    return memo;
+  }, {
+    __NODE_ENV__: JSON.stringify(NODE_ENV),
+    __DEBUG__: isDev
+  });
+
+config.plugins = [
+  new webpack.DefinePlugin(defines)
+].concat(config.plugins);
+// END ENV variables
+
+// CSS modules
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l && l.loader && l.loader.match(match));
+  return found ? found[0] : null;
+};
+
+// existing css loader
+const cssloader =
+  findLoader(config.module.loaders, matchCssLoaders);
+
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader.replace(matchCssLoaders, `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+});
+config.module.loaders.push(newloader);
+cssloader.test = new RegExp(`[^module]${cssloader.test.source}`);
+cssloader.loader = newloader.loader;
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+});
+
+// CSS modules
+
+// postcss
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+]);
+
+// END postcss
+
+// Roots
+config.resolve.root = [src, modules];
+config.resolve.alias = {
+  css: join(src, 'styles'),
+  containers: join(src, 'containers'),
+  components: join(src, 'components'),
+  utils: join(src, 'utils'),
+  styles: join(src, 'styles')
+};
+// end Roots
+
+// Dev
+if (isDev) {
+  config.devServer.port = devPort;
+  config.devServer.hostname = devHost;
 }
+
+// Testing
+if (isTest) {
+  config.externals = {
+    'react/addons': true,
+    'react/lib/ReactContext': true,
+    'react/lib/ExecutionEnvironment': true
+  };
+
+  config.module.noParse = /[/\\]sinon\.js/;
+  config.resolve.alias.sinon = 'sinon/pkg/sinon';
+
+  config.plugins = config.plugins.filter(p => {
+    const name = p.constructor.toString();
+    const fnName = name.match(/^function (.*)\((.*\))/);
+
+    const idx = [
+      'DedupePlugin',
+      'UglifyJsPlugin'
+    ].indexOf(fnName[1]);
+    return idx < 0;
+  });
+}
+// End Testing
 
 module.exports = config;
